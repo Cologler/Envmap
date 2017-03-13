cb36c33855543f0ea8c93c8d92d97e4f1b353f0e
赖巍 <skyoflw@gmail.com>
2017-03-14 03:11:04 +0800

    init.

---
+++ Envmap/EnvmapRuntimeException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Envmap
+{
+    public class EnvmapRuntimeException : Exception
+    {
+        public EnvmapRuntimeException(string message) : base(message) { }
+    }
+}
--- Envmap/Program.cs
+++ Envmap/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,33 @@ namespace Envmap
     {
         static void Main(string[] args)
         {
+            var builder = new CommandBuilder();
+            try
+            {
+                builder.ReadFromArgs(args);
+            }
+            catch (ArgumentException error)
+            {
+                Console.WriteLine(error.Message);
+                CommandBuilder.PrintUsage();
+                return;
+            }
+            try
+            {
+                builder.Build().Invoke();
+            }
+            catch (EnvmapRuntimeException error)
+            {
+                Console.WriteLine("<Error>: " + error.Message);
+            }
+        }
+
+        static void Map(string path)
+        {
+            var editor = new PathEnvironmentVariableEditor("PATH", EnvironmentVariableTarget.User);
+            editor.LoadFromTarget();
+            editor.Map(path);
+            Console.WriteLine(editor.ToString());
         }
     }
 }
